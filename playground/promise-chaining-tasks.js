4201e38b99d07eaf1ac8fb94485d9708c79fb8fd
Jason Marshall <jasonnz@gmail.com>
2020-01-04 20:26:06 +1300

    Refactored files from promise chaining to async/await

--- playground/promise-chaining-tasks.js
+++ playground/promise-chaining-tasks.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e08567fcd237c13cdec7849').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed:true })
-}).then((task) => {
-    console.log(task)  
+// Task.findByIdAndDelete('5e08567fcd237c13cdec7849').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed:true })
+// }).then((task) => {
+//     console.log(task)  
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5e0c64cec3d3aa13048330c6').then((count) => {
+    console.log(count)
 }).catch((error) => {
     console.log(error)
 })
--- src/index.js
+++ src/index.js
@@ -4,104 +4,139 @@ const express = require('express')
 require('./db/mongoose')
 
 const User = require('./models/user')
-const Task = require("./models/task");
+const Task = require("./models/task")
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json());
+app.use(express.json())
 
-app.get('/users', (req, res) => {
-  User.find({}).then((users) => {
-    res.status(200);
-    res.send(users)
-  }).catch((error) => {
-    res.status(400)
-    res.send(`${error}`)
-  })
+
+app.get('/users', async (req, res) => {
+
+  // User.find({}).then((users) => {
+  //   res.status(200)
+  //   res.send(users)
+  // }).catch((error) => {
+  //   res.status(400)
+  //   res.send(`${error}`)
+  // })
+
+  try {
+    const user = await User.find({})
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id
   if (!_id.match(/^[0-9a-fA-F]{24}$/)) return res.status(404).send({ error: 'Not a valid _id!' })
-  User.findById(_id).then((user) => {
+
+  // User.findById(_id).then((user) => {
+  //   if (!user) return res.status(404).send({ error: 'No user found!' })
+  //   res.status(200)
+  //   res.send(user)
+  // }).catch((error) => {
+  //   res.status(500)
+  //   res.send(`${error}`)
+  // })
+
+  try {
+    const user = await User.findById(_id)
     if (!user) return res.status(404).send({ error: 'No user found!' })
-    res.status(200);
+    res.status(200)
     res.send(user)
-  }).catch((error) => {
-    res.status(500)
-    res.send(`${error}`)
-  })
+  } catch (error) {
+    res.status(500).send(error)
+  }
+
 })
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    user.save().then((user) => {
-        res.status(200);
-        res.send(user)
-    }).catch((error) => {
-        res.status(500)
-        res.send(`${error}`)
-    })
+    
+    // user.save().then((user) => {
+    //     res.status(201)
+    //     res.send(user)
+    // }).catch((error) => {
+    //     res.status(500)
+    //     res.send(`${error}`)
+    // })
+
+    try {
+      await user.save()
+      res.status(201).send(user)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+
 })
 
-app.get('/users', (req, res) => {
-  User.find({}).then((users) => {
-    res.status(200);
-    res.send(users)
-  }).catch((error) => {
-    res.status(400)
-    res.send(`${error}`)
-  })
+app.get('/tasks', async (req, res) => {
+  // Task.find({}).then((task) => {
+  //   res.status(200)
+  //   res.send(task)
+  // }).catch((error) => {
+  //   res.status(400)
+  //   res.send(`${error}`)
+  // })
+
+  try {
+    const task = await Task.find({})
+    res.status(200).send(task)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id
   if (!_id.match(/^[0-9a-fA-F]{24}$/)) return res.status(404).send({ error: 'Not a valid _id!' })
-  User.findById(_id).then((user) => {
-    if (!user) return res.status(404).send({ error: 'No user found!' })
-    res.status(200);
-    res.send(user)
-  }).catch((error) => {
-    res.status(500)
-    res.send(`${error}`)
-  })
-})
+  // Task.findById(_id).then((task) => {
+  //   if (!task) return res.status(404).send({ error: 'No task found!' })
+  //   res.status(200)
+  //   res.send(task)
+  // }).catch((error) => {
+  //   res.status(500)
+  //   res.send(`${error}`)
+  // })
 
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body)
-    task.save().then(() => {
-      res.status(201);
-      res.send(task);
-    }).catch(error => {
-      res.status(400);
-      res.send(`${error}`);
-    });
-}); 
-
-app.get('/tasks', (req, res) => {
-  Task.find({}).then((task) => {
-    res.status(200);
-    res.send(task)
-  }).catch((error) => {
-    res.status(400)
-    res.send(`${error}`)
-  })
-})
-
-app.get('/tasks/:id', (req, res) => {
-  const _id = req.params.id
-  if (!_id.match(/^[0-9a-fA-F]{24}$/)) return res.status(404).send({ error: 'Not a valid _id!' })
-  Task.findById(_id).then((task) => {
+  try {
+    const task = await Task.findById(_id)
     if (!task) return res.status(404).send({ error: 'No task found!' })
-    res.status(200);
+    res.status(200)
     res.send(task)
-  }).catch((error) => {
-    res.status(500)
-    res.send(`${error}`)
-  })
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+app.post("/tasks", async (req, res) => {
+
+  // const task = new Task(req.body)
+  //   task.save().then(() => {
+  //     res.status(201)
+  //     res.send(task)
+  //   }).catch(error => {
+  //     res.status(400)
+  //     res.send(`${error}`)
+  //   })
+
+  const task = new Task(req.body)
+
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+
 })
 
 
 app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`);
+    console.log(`Server is up and running on port ${port}`)
 })
