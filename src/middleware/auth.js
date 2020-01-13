af467c5466d7e803884c7e5e769c849303465393
Jason Marshall <jasonnz@gmail.com>
2020-01-13 21:27:33 +1300

    Added delete cascade middleware function in mongoose

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        console.log('token = ' + token)
+        // console.log('token = ' + token)
         const decoded = jwt.verify(token, 'abc1234')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token});
         // console.log('user = ' + user)
--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@ const validator = require("validator")
 const mongoose = require("mongoose")
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -48,6 +49,8 @@ const userSchema = new mongoose.Schema({
       required: true
     }
   }]
+}, {
+  timestamps: true
 })
 
 userSchema.virtual('tasks', {
@@ -99,6 +102,15 @@ userSchema.pre('save', async function (next) {
     user.password = await bcrypt.hash(user.password, 8)
   }
   
+  next()
+})
+
+// Delete users tasks when user is removed
+userSchema.pre('remove', async function (next) {
+  const user = this
+
+  await Task.deleteMany({owner: user._id})
+
   next()
 })
 
