fb50ad2e447a9bd490d9a967e93deafe5eceb80f
Jason Marshall <jasonnz@gmail.com>
2020-01-31 20:40:20 +1300

    Added Jest test cuite

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require("../src/math");
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -6,6 +6,16 @@ test('Should calculate total with tip', () => {
 })
 
 test('Should calculate total with default tip', () => {
-  const total = calculateTip(10);
-  expect(total).toBe(12.5);
+  const total = calculateTip(10)
+  expect(total).toBe(12.5)
+})
+
+test('Should convert 32 F to 0 C', () => {
+  const total = fahrenheitToCelsius(32)
+  expect(total).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+  const total = celsiusToFahrenheit(0)
+  expect(total).toBe(32)
 })
